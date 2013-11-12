fa4a7ee304334311dc1eaecaf35dd6b05d0b625a
Ronan McHugh <mchugh.r@gmail.com>
2013-11-12 11:00:00 +0100

    Localisation code in default controller.

--- app/controllers/umlaut_controller.rb
+++ app/controllers/umlaut_controller.rb
@@ -23,6 +23,26 @@ class UmlautController < ApplicationController
     # implementation for list. Configuration actually uses
     # a Confstruct object.
 
+
+    # Code to include locale in all links
+
+    #before_filter :set_locale
+
+    # if no locale is set, will default to default_locale (i.e. en)
+    # def set_locale
+    #   I18n.locale = params['umlaut.locale'.to_sym] || I18n.default_locale
+    # end
+
+    # ensure locale is always included in any internal links
+    # def default_url_options(options={})
+    #   { 'umlaut.locale'.to_sym => I18n.locale }
+    # end
+
+
+
+
+
+
     umlaut_config.configure do
       # app_name 'Find It'
 
